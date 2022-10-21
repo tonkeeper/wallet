30a729e0761b18fe52861fc0ad5be6c5aa61a36d
simonvar <simonvar@protonmail.com>
2022-10-21 18:51:28 +0400

    Android: update mnemonic save

--- android/app/src/main/java/com/ton_keeper/walletstore/WalletStore.kt
+++ android/app/src/main/java/com/ton_keeper/walletstore/WalletStore.kt
@@ -3,11 +3,13 @@ package com.ton_keeper.walletstore
 import android.content.Context
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import android.util.Base64
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.fragment.app.FragmentActivity
 import com.ton_keeper.crypto.Mnemonic
 import com.ton_keeper.crypto.toHex
+import com.ton_keeper.walletstore.mnemonic.MnemonicSerializer
 import com.ton_keeper.walletstore.passcode.PasscodeManager
 import com.ton_keeper.walletstore.walletinfo.PublicKey
 import com.ton_keeper.walletstore.walletinfo.WalletInfo
@@ -24,16 +26,15 @@ class WalletStore(activity: FragmentActivity) {
     private val prefs = activity.getSharedPreferences(PrefsFileName, Context.MODE_PRIVATE)
     private val passcode = PasscodeManager(activity)
 
-    fun import(mnemonic: Array<String>, secure: SecureType, onResult: (WalletInfo) -> Unit) {
-        val keys = Mnemonic.toKeyPair(mnemonic)
+    fun import(mnemonic: List<String>, secure: SecureType, onResult: (WalletInfo) -> Unit) {
+        val keys = Mnemonic.toKeyPair(mnemonic.toTypedArray())
         val pkHex = keys.publicKey.toHex()
 
         authenticate(
             secure = secure,
             onAccess = {
-                encryptMnemonic(mnemonic, pkHex) {
-                    onResult(WalletInfo(pk = pkHex, label = ""))
-                }
+                encryptMnemonic(mnemonic, pkHex)
+                onResult(WalletInfo(pk = pkHex, label = ""))
             },
             onFailure = { throw WalletInvalidAuth() }
         )
@@ -52,10 +53,6 @@ class WalletStore(activity: FragmentActivity) {
 
         val jsonStr = prefs.getString(key, null) ?: throw WalletNotFoundException(pk)
         return WalletInfoSerializer.deserializeFromString(jsonStr)
-    }
-
-    fun getByAddress(address: String): WalletInfo {
-        return TODO("Implement when Tonlib will be ready")
     }
 
     fun updateLabelByPublicKey(pk: PublicKey, label: String): WalletInfo {
@@ -77,10 +74,9 @@ class WalletStore(activity: FragmentActivity) {
         authenticate(
             secure = secure,
             onAccess = {
-                decryptMnemonic(pk) {
-                    val keys = Mnemonic.toKeyPair(it.toTypedArray())
-                    onResult(keys.secretKey)
-                }
+                val mnemonic = decryptMnemonic(pk)
+                val keys = Mnemonic.toKeyPair(mnemonic.toTypedArray())
+                onResult(keys.secretKey)
             },
             onFailure = { throw WalletInvalidAuth() }
         )
@@ -89,7 +85,10 @@ class WalletStore(activity: FragmentActivity) {
     fun backup(pk: PublicKey, secure: SecureType, onResult: (List<String>) -> Unit) {
         authenticate(
             secure = secure,
-            onAccess = { decryptMnemonic(pk) { onResult(it) } },
+            onAccess = {
+                val mnemonic = decryptMnemonic(pk)
+                onResult(mnemonic)
+            },
             onFailure = { throw WalletInvalidAuth() }
         )
     }
@@ -115,14 +114,33 @@ class WalletStore(activity: FragmentActivity) {
         }
     }
 
-    private fun encryptMnemonic(mnemonic: Array<String>, pk: PublicKey, onResult: () -> Unit) {
+    private fun encryptMnemonic(mnemonic: List<String>, pk: PublicKey) {
         val cipher = getCipher()
         val secretKey = getOrCreateSecretKey()
         cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+
+        val mnemonicStr = MnemonicSerializer.serializeToString(mnemonic)
+        val encrypted = cipher.doFinal(mnemonicStr.toByteArray())
+        val encoded = Base64.encodeToString(encrypted, Base64.DEFAULT)
+
+        val key = createMnemonicPrefsAlias(pk)
+        val result = prefs.edit().putString(key, encoded).commit()
+        if (!result) throw WalletSaveException(pk)
     }
 
-    private fun decryptMnemonic(pk: PublicKey, onResult: (List<String>) -> Unit) {
+    private fun decryptMnemonic(pk: PublicKey): List<String> {
+        val key = createMnemonicPrefsAlias(pk)
+        if (prefs.contains(key).not()) throw WalletNotFoundException(pk)
 
+        val encoded = prefs.getString(key, null) ?: throw WalletNotFoundException(pk)
+        val encrypted = Base64.decode(encoded, Base64.DEFAULT)
+
+        val cipher = getCipher()
+        val secretKey = getOrCreateSecretKey()
+        cipher.init(Cipher.DECRYPT_MODE, secretKey)
+
+        val mnemonicStr = String(cipher.doFinal(encrypted))
+        return MnemonicSerializer.deserializeFromString(mnemonicStr)
     }
 
     private fun createAuthenticationCallback(
