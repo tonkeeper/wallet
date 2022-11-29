8afa897f1d204245c9cc6457cd78a412b7c0d9e3
simonvar <simonvar@protonmail.com>
2022-11-29 15:34:07 +0300

    Android: add devkit kotlin source set

---
+++ android/devkit/sample/src/main/kotlin/com/tonkeeper/devkit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tonkeeper.devkit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
