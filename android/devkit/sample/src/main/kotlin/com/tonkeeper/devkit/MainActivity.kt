044eac06668d7e86f493e80b6b1ffb84b0722eb3
simonvar <simonvar@protonmail.com>
2022-11-29 19:17:07 +0300

    Android: devkit sample setup edge to edge

--- android/devkit/sample/src/main/kotlin/com/tonkeeper/devkit/MainActivity.kt
+++ android/devkit/sample/src/main/kotlin/com/tonkeeper/devkit/MainActivity.kt
@@ -2,11 +2,17 @@ package com.tonkeeper.devkit
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setEdgeToEdge()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setEdgeToEdge() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
