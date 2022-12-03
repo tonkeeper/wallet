b9320ebed40fff0fa8f577746f34dc20cbeae6a5
simonvar <simonvar@protonmail.com>
2022-12-03 21:48:27 +0300

    Android: add compose to devkit sample

--- android/devkit/sample/build.gradle.kts
+++ android/devkit/sample/build.gradle.kts
@@ -38,7 +38,21 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
+
     implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.compose.material3:material3:1.0.1")
+
+    val compose = "1.3.2"
+    implementation("androidx.compose.ui:ui:$compose")
+    implementation("androidx.compose.ui:ui-util:$compose")
+    implementation("androidx.compose.ui:ui-text:$compose")
+    implementation("androidx.compose.ui:ui-tooling-preview:$compose")
+    implementation("androidx.compose.foundation:foundation:$compose")
+    implementation("androidx.compose.foundation:foundation-layout:$compose")
+    implementation("androidx.compose.animation:animation:$compose")
+    implementation("androidx.compose.material:material-icons-core:$compose")
+    implementation("androidx.compose.material:material-icons-extended:$compose")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
