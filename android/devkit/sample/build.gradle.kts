a84c9796ce4c97ceb25f75ef336af05a19949734
simonvar <simonvar@protonmail.com>
2022-12-06 15:23:02 +0300

    Android: add compose bom dependency

--- android/devkit/sample/build.gradle.kts
+++ android/devkit/sample/build.gradle.kts
@@ -30,6 +30,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
 }
 
 dependencies {
@@ -38,22 +44,32 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
-
     implementation("com.google.android.material:material:1.7.0")
-    implementation("androidx.compose.material3:material3:1.0.1")
 
-    val compose = "1.3.2"
-    implementation("androidx.compose.ui:ui:$compose")
-    implementation("androidx.compose.ui:ui-util:$compose")
-    implementation("androidx.compose.ui:ui-text:$compose")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose")
-    implementation("androidx.compose.foundation:foundation:$compose")
-    implementation("androidx.compose.foundation:foundation-layout:$compose")
-    implementation("androidx.compose.animation:animation:$compose")
-    implementation("androidx.compose.material:material-icons-core:$compose")
-    implementation("androidx.compose.material:material-icons-extended:$compose")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+
+    val composeBom = platform("androidx.compose:compose-bom:2022.10.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-util")
+    implementation("androidx.compose.ui:ui-text")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.foundation:foundation-layout")
+    implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3:1.0.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+
+    // Debug
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- android/devkit/sample/src/main/java/com/tonkeeper/devkit/MainActivity.kt
+++ android/devkit/sample/src/main/java/com/tonkeeper/devkit/MainActivity.kt
@@ -2,6 +2,11 @@ package com.tonkeeper.devkit
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.core.view.WindowCompat
 import com.tonkeeper.ton.mnemonic.Mnemonic
 import com.tonkeeper.ton.mnemonic.Wordlist
@@ -11,10 +16,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContent {
+            MaterialTheme {
+                Home()
+            }
+        }
     }
 
     private fun setEdgeToEdge() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
+    @Composable
+    private fun Home() {
+        Column {
+
+        }
     }
 }
