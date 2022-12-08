c270a8878ba477dde289fc927c44663ab64cc5cc
simonvar <simonvar@protonmail.com>
2022-12-08 18:29:14 +0300

    Android: add features dependencies to devkit sample

--- android/devkit/sample/build.gradle.kts
+++ android/devkit/sample/build.gradle.kts
@@ -41,10 +41,14 @@ android {
 dependencies {
     implementation(project(":libs-ton:crypto"))
     implementation(project(":libs-ton:mnemonic"))
+    implementation(project(":libs-feature:localauth"))
+    implementation(project(":libs-feature:wallet"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.7.0")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
