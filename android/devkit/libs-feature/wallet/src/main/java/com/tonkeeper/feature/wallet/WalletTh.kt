c7e5f0a89795c16449b49902e30c007f52712177
simonvar <simonvar@protonmail.com>
2022-12-23 17:44:20 +0300

    Android: implement wallet selection repository

--- android/devkit/libs-feature/wallet/src/main/java/com/tonkeeper/feature/wallet/WalletTh.kt
+++ android/devkit/libs-feature/wallet/src/main/java/com/tonkeeper/feature/wallet/WalletTh.kt
@@ -3,6 +3,9 @@ package com.tonkeeper.feature.wallet
 class WalletMnemonicInvalidException
     : Exception("Mnemonic of wallet is invalid.")
 
+class WalletEmptyException
+    : Exception("Can not perform an operation because wallet is empty.")
+
 class WalletNotFoundException(
     publicKey: String
 ) : Exception("Wallet with public key not found (pk: $publicKey).")
