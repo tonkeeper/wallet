614226187e4cad5311b789c4011ed6e4e98f7efa
simonvar <simonvar@protonmail.com>
2023-01-11 18:46:46 +0300

    Android: roll back to 32 sdk

--- android/devkit/libs-feature/localauth/build.gradle.kts
+++ android/devkit/libs-feature/localauth/build.gradle.kts
@@ -36,7 +36,11 @@ android {
 dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.datastore:datastore:1.0.0")
-    implementation("androidx.biometric:biometric-ktx:1.2.0-alpha05")
+    implementation("androidx.biometric:biometric:1.1.0")
+
+    // Local tests
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+
+    // Instrumented tests
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 }
