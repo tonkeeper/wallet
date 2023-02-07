9d0cf84dbf9a1fb4ba7b265de04807310e36461f
bogoslavskiy <luice96@yahoo.com>
2023-02-07 18:32:42 +0400

    chore(web): add shared package to web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TestCube } from '@tonkeeper/shared/components/uikit/TestCube';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <TestCube />
         <a
           className="App-link"
           href="https://reactjs.org"
