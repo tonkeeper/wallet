6198da0c497ba28d3ce29b84dcbfe6173dc95082
bogoslavskiy <luice96@yahoo.com>
2023-02-07 06:48:10 +0400

    wip

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,10 +1,15 @@
 const { getDefaultConfig } = require('metro-config');
+const path = require('path');
 
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
   return {
+    watchFolders: [
+      path.resolve(__dirname, '../../node_modules'),
+      // path.resolve(__dirname, '../../node_modules/@tonkeeper/shared'),
+    ],
     transformer: {
       getTransformOptions: async () => ({
         transform: {
