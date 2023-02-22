1bd304b9bf167f995e17c22a58f00d298a0799ee
Max Voloshinskii <me@voloshinskii.ru>
2023-02-22 18:53:05 +0400

    Chart fixes

--- packages/mobile/src/uikit/Chart/Chart.tsx
+++ packages/mobile/src/uikit/Chart/Chart.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { ChartDot, ChartPath, ChartPathProvider, CurrentPositionVerticalLine } from '@rainbow-me/animated-charts';
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+  CurrentPositionVerticalLine,
+} from '@rainbow-me/animated-charts';
 import { Dimensions, View } from 'react-native';
 import { useTheme } from '$hooks';
 import { useSelector } from 'react-redux';
@@ -21,81 +26,137 @@ export const { width: SIZE } = Dimensions.get('window');
 export const DEFAULT_CHART_PERIOD = ChartPeriod.ONE_DAY;
 
 export const Chart: React.FC = () => {
-    const theme = useTheme();
-    const [selectedPeriod, setSelectedPeriod] = useState<ChartPeriod>(DEFAULT_CHART_PERIOD);
+  const theme = useTheme();
+  const [selectedPeriod, setSelectedPeriod] = useState<ChartPeriod>(DEFAULT_CHART_PERIOD);
 
-    const { isLoading, isFetching, data } = useQuery(['chartFetch', selectedPeriod], () => loadChartData(selectedPeriod));
+  const { isLoading, isFetching, data } = useQuery(['chartFetch', selectedPeriod], () =>
+    loadChartData(selectedPeriod),
+  );
 
-    const [cachedData, setCachedData] = useState([]);
+  const [cachedData, setCachedData] = useState([]);
 
-    useEffect(() => {
-        if (data) {
-            setCachedData(data.data);
-        }
-    }, [data])
+  useEffect(() => {
+    if (data) {
+      setCachedData(data.data);
+    }
+  }, [data]);
 
-    const rates = useSelector(ratesRatesSelector);
-    const fiatCurrency = useSelector(fiatCurrencySelector);
+  const rates = useSelector(ratesRatesSelector);
+  const fiatCurrency = useSelector(fiatCurrencySelector);
 
-    const fiatRate =
+  const fiatRate =
     fiatCurrency === FiatCurrencies.Usd
       ? 1
       : getRate(rates, CryptoCurrencies.Usdt, fiatCurrency);
-      
-    const [max, min] = React.useMemo(() => {
-        if (!cachedData.length) return [0, 0];
-        const mappedPoints = cachedData.map(o => o.y);
-        return [
-            Math.max(...mappedPoints), Math.min(...mappedPoints)
-        ].map(value => formatFiatCurrencyAmount((value * fiatRate).toFixed(2), fiatCurrency));
-    }, [fiatCurrency, cachedData]);
 
-    const [firstPoint, latestPoint] = React.useMemo(() => {
-        if (!cachedData.length) return [0, 0];
-        const latest = cachedData[cachedData.length - 1].y;
-        const first = cachedData[0].y;
-        return [first, latest];
-    }, [cachedData]);
+  const [max, min] = React.useMemo(() => {
+    if (!cachedData.length) {
+      return [0, 0];
+    }
+    const mappedPoints = cachedData.map((o) => o.y);
+    return [Math.max(...mappedPoints), Math.min(...mappedPoints)].map((value) =>
+      formatFiatCurrencyAmount((value * fiatRate).toFixed(2), fiatCurrency),
+    );
+  }, [fiatCurrency, cachedData]);
 
-    if (isLoading && !cachedData) return null;
+  const [firstPoint, latestPoint] = React.useMemo(() => {
+    if (!cachedData.length) {
+      return [0, 0];
+    }
+    const latest = cachedData[cachedData.length - 1].y;
+    const first = cachedData[0].y;
+    return [first, latest];
+  }, [cachedData]);
 
-    return (
-        <View>
+  if (isLoading && !cachedData) {
+    return null;
+  }
+
+  return (
+    <View>
+      <View>
+        <ChartPathProvider data={{ points: cachedData }}>
+          <View style={{ paddingHorizontal: 28 }}>
+            {latestPoint ? (
+              <Rate
+                fiatCurrency={fiatCurrency}
+                fiatRate={fiatRate}
+                latestPoint={latestPoint}
+              />
+            ) : null}
+            {latestPoint ? (
+              <PercentDiff
+                fiatCurrency={fiatCurrency}
+                fiatRate={fiatRate}
+                latestPoint={latestPoint}
+                firstPoint={firstPoint}
+              />
+            ) : null}
+            <PriceLabel selectedPeriod={selectedPeriod} />
+          </View>
+          <View style={{ paddingVertical: 15 }}>
             <View>
-                <ChartPathProvider data={{ points: cachedData }}>
-                    <View style={{ paddingHorizontal: 28 }}>
-                        {latestPoint ? <Rate fiatCurrency={fiatCurrency} fiatRate={fiatRate} latestPoint={latestPoint} /> : null}
-                        {latestPoint ? <PercentDiff fiatCurrency={fiatCurrency} fiatRate={fiatRate} latestPoint={latestPoint} firstPoint={firstPoint} /> : null}
-                        <PriceLabel selectedPeriod={selectedPeriod} />
-                    </View>
-                    <View style={{ paddingVertical: 15 }}>
-                        <View>
-                            <ChartPath 
-                                gradientEnabled 
-                                longPressGestureHandlerProps={{ minDurationMs: 90 }} 
-                                hapticsEnabled 
-                                strokeWidth={2} 
-                                selectedStrokeWidth={2}
-                                height={190} 
-                                stroke={theme.colors.accentPrimaryLight}
-                                width={SIZE}
-                                selectedOpacity={1}
-                            />
-                            <ChartDot size={40} style={{ backgroundColor: 'rgba(69,174,245,0.24)', alignItems: 'center', justifyContent: 'center' }}>
-                                <View style={{ height: 16, width: 16, backgroundColor: theme.colors.accentPrimaryLight, borderRadius: 8 }} />
-                            </ChartDot>
-                            <CurrentPositionVerticalLine thickness={2} strokeDasharray={0} length={230} color={theme.colors.accentPrimaryLight} />
-                        </View>
-                    </View>
-                </ChartPathProvider>
-                <View style={{ position: 'absolute', right: 18, bottom: 24 }}>
-                    <Text style={{ fontFamily: 'SFMono-Medium' }} variant='label3' color='foregroundSecondary'>{min}</Text>
-                </View>
-                <View style={{ position: 'absolute', right: 18, top: 68 }}>
-                    <Text style={{ fontFamily: 'SFMono-Medium' }} variant='label3' color='foregroundSecondary'>{max}</Text>
-                </View>
+              <ChartPath
+                gradientEnabled
+                longPressGestureHandlerProps={{ minDurationMs: 90 }}
+                hapticsEnabled
+                strokeWidth={2}
+                selectedStrokeWidth={2}
+                height={170}
+                stroke={theme.colors.accentPrimaryLight}
+                width={SIZE}
+                selectedOpacity={1}
+              />
+              <ChartDot
+                size={40}
+                style={{
+                  backgroundColor: 'rgba(69,174,245,0.24)',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <View
+                  style={{
+                    height: 16,
+                    width: 16,
+                    backgroundColor: theme.colors.accentPrimaryLight,
+                    borderRadius: 8,
+                  }}
+                />
+              </ChartDot>
+              <CurrentPositionVerticalLine
+                thickness={2}
+                strokeDasharray={0}
+                length={230}
+                color={theme.colors.accentPrimaryLight}
+              />
             </View>
-            <PeriodSelector disabled={isLoading || isFetching} selectedPeriod={selectedPeriod} onSelect={setSelectedPeriod} />
+          </View>
+        </ChartPathProvider>
+        <View style={{ position: 'absolute', right: 18, bottom: 2 }}>
+          <Text
+            style={{ fontFamily: 'SFMono-Medium' }}
+            variant="label3"
+            color="foregroundSecondary"
+          >
+            {min}
+          </Text>
         </View>
-    )
-}
+        <View style={{ position: 'absolute', right: 18, top: 68 }}>
+          <Text
+            style={{ fontFamily: 'SFMono-Medium' }}
+            variant="label3"
+            color="foregroundSecondary"
+          >
+            {max}
+          </Text>
+        </View>
+      </View>
+      <PeriodSelector
+        disabled={isLoading || isFetching}
+        selectedPeriod={selectedPeriod}
+        onSelect={setSelectedPeriod}
+      />
+    </View>
+  );
+};
--- packages/mobile/src/uikit/Chart/PercentDiff.tsx/PercentDiff.tsx
+++ packages/mobile/src/uikit/Chart/PercentDiff.tsx/PercentDiff.tsx
@@ -10,10 +10,10 @@ import { useChartData } from '@rainbow-me/animated-charts';
 import { Platform } from 'react-native';
 
 export interface PercentDiffProps {
-    latestPoint: number;
-    firstPoint: number;
-    fiatRate: number;
-    fiatCurrency: FiatCurrencies;
+  latestPoint: number;
+  firstPoint: number;
+  fiatRate: number;
+  fiatCurrency: FiatCurrencies;
 }
 
 const fontFamily = Platform.select({
@@ -22,64 +22,75 @@ const fontFamily = Platform.select({
 });
 
 export const PercentDiff: React.FC<PercentDiffProps> = (props) => {
-    const chartData = useChartData();
-    const theme = useTheme();
-    const [activePoint, setActivePoint] = React.useState(props.latestPoint);
+  const chartData = useChartData();
+  const theme = useTheme();
+  const [activePoint, setActivePoint] = React.useState(props.latestPoint);
 
-    const priceDiff = React.useMemo(() => {
-        return (((activePoint - props.firstPoint) / props.firstPoint) * 100).toFixed(
-            2,
-          );
-    }, [activePoint, props.firstPoint]);
+  const priceDiff = React.useMemo(() => {
+    return (((activePoint - props.firstPoint) / props.firstPoint) * 100).toFixed(2);
+  }, [activePoint, props.firstPoint]);
 
-    useEffect(() => {
+  useEffect(() => {
+    setActivePoint(props.latestPoint);
+  }, [props.latestPoint]);
+
+  const fiatInfo = React.useMemo(() => {
+    let percent = '0.0%';
+    let color: TonThemeColor = 'foregroundSecondary';
+    let amountResult: string;
+
+    const diffInFiat = formatFiatCurrencyAmount(
+      Math.abs(((activePoint * parseFloat(priceDiff)) / 100) * props.fiatRate).toFixed(2),
+      props.fiatCurrency,
+    );
+
+    percent =
+      priceDiff === null
+        ? '-'
+        : (+priceDiff > 0 ? '+ ' : '- ') + Math.abs(Number(priceDiff)) + '%';
+    if (priceDiff !== null) {
+      color = +priceDiff > 0 ? 'accentPositive' : 'accentNegative';
+    }
+
+    percent = toLocaleNumber(percent);
+
+    return {
+      percent,
+      diffInFiat,
+      color,
+    };
+  }, [priceDiff, theme.colors, activePoint]);
+
+  const formatPriceWrapper = (point: number) => {
+    if (!point) {
       setActivePoint(props.latestPoint);
-    }, [props.latestPoint]);
-    
-      const fiatInfo = React.useMemo(() => {
-        let percent = '0.0%';
-        let color: TonThemeColor = 'foregroundSecondary';
-        let amountResult: string;
+      return;
+    }
+    setActivePoint(point);
+  };
 
-        const diffInFiat = formatFiatCurrencyAmount(Math.abs(((activePoint * parseFloat(priceDiff) / 100) * props.fiatRate)).toFixed(2), props.fiatCurrency);
-    
-        percent = priceDiff === null ? '-' : (+priceDiff > 0 ? '+ ' : '-') + Math.abs(Number(priceDiff)) + '%';
-        if (priceDiff !== null) {
-          color = +priceDiff > 0 ? 'accentPositive' : 'accentNegative';
-        }
-    
-        percent = toLocaleNumber(percent);
-    
-        return {
-          percent,
-          diffInFiat,
-          color,
-        };
-      }, [priceDiff, theme.colors, activePoint]);
+  useAnimatedReaction(
+    () => {
+      return chartData?.originalY.value;
+    },
+    (result, previous) => {
+      if (result !== previous) {
+        runOnJS(formatPriceWrapper)(result);
+      }
+    },
+    [props.latestPoint],
+  );
 
-      const formatPriceWrapper = (point: number) => {
-        if (!point) {
-            setActivePoint(props.latestPoint);
-            return;
-        }
-        setActivePoint(point);
-      };
-
-      useAnimatedReaction(() => {
-          return chartData?.originalY.value;
-      }, (result, previous) => {
-          if (result !== previous) {
-            runOnJS(formatPriceWrapper)(result);
-          }
-      }, [props.latestPoint]);
-    
-
-    return (
-        <Text variant='body2' color={fiatInfo.color} style={{ fontFamily }}>
-            {fiatInfo.percent}{' '}
-            <Text variant='body2' color={fiatInfo.color} style={{ opacity: 0.42, fontFamily, marginLeft: 8 }}>
-                {fiatInfo.diffInFiat}
-            </Text>
-        </Text>
-    )
-}
+  return (
+    <Text variant="body2" color={fiatInfo.color} style={{ fontFamily }}>
+      {fiatInfo.percent}{' '}
+      <Text
+        variant="body2"
+        color={fiatInfo.color}
+        style={{ opacity: 0.42, fontFamily, marginLeft: 8 }}
+      >
+        {fiatInfo.diffInFiat}
+      </Text>
+    </Text>
+  );
+};
