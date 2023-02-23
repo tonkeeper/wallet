1e3c7ff0392e5a279882292309f50957cb06b765
Max Voloshinskii <me@voloshinskii.ru>
2023-02-23 04:44:25 +0400

    Don't show hours label on monthly chart

--- packages/mobile/src/uikit/Chart/PriceLabel/PriceLabel.tsx
+++ packages/mobile/src/uikit/Chart/PriceLabel/PriceLabel.tsx
@@ -9,36 +9,61 @@ import { Platform } from 'react-native';
 import { ChartPeriod } from '../Chart.types';
 
 const fontFamily = Platform.select({
-    ios: 'SFMono-Medium',
-    android: 'RobotoMono-Medium',
+  ios: 'SFMono-Medium',
+  android: 'RobotoMono-Medium',
 });
 
 export const PriceLabel: React.FC<{ selectedPeriod: ChartPeriod }> = (props) => {
-    const shouldShowYearLabel = useMemo(() => [ChartPeriod.ONE_MONTH, ChartPeriod.ONE_HOUR, ChartPeriod.ONE_DAY, ChartPeriod.SEVEN_DAYS].includes(props.selectedPeriod), [props.selectedPeriod]);
-    const chartData = useChartData();
-    const [state, setState] = useState('');
+  const shouldShowHoursLabel = useMemo(
+    () =>
+      [ChartPeriod.ONE_HOUR, ChartPeriod.ONE_DAY, ChartPeriod.SEVEN_DAYS].includes(
+        props.selectedPeriod,
+      ),
+    [props.selectedPeriod],
+  );
+  const chartData = useChartData();
+  const [state, setState] = useState('');
 
+  const formatDateWrapper = useCallback(
+    (text: string) => {
+      if (!text) {
+        setState(t('chart.price'));
+        return;
+      }
+      const subbedText = subMonths(new Date(parseInt(text) * 1000), 1);
+      setState(
+        format(
+          subbedText,
+          `${getLocale().code === 'ru' ? 'cccccc' : 'ccc'}, dd MMM ${
+            shouldShowHoursLabel ? 'H:mm' : ''
+          }`,
+          { locale: getLocale() },
+        ),
+      );
+    },
+    [shouldShowHoursLabel],
+  );
 
-    const formatDateWrapper = useCallback((text: string) => {
-        if (!text) {
-            setState(t('chart.price'));
-            return;
-        }
-        const subbedText = subMonths(new Date(parseInt(text) * 1000), 1);
-        setState(format(subbedText, `${getLocale().code === 'ru' ? 'cccccc' : 'ccc'}, dd MMM ${shouldShowYearLabel ? 'H:mm' : ''}`, { locale: getLocale() }));
-    }, [shouldShowYearLabel]);
+  useAnimatedReaction(
+    () => {
+      return chartData?.originalX.value;
+    },
+    (result, previous) => {
+      if (result !== previous) {
+        runOnJS(formatDateWrapper)(result);
+      }
+    },
+    [formatDateWrapper],
+  );
 
-    useAnimatedReaction(() => {
-        return chartData?.originalX.value;
-    }, (result, previous) => {
-        if (result !== previous) {
-           runOnJS(formatDateWrapper)(result);
-        }
-    }, [formatDateWrapper]);
-
-    return (
-        <Text style={{ fontFamily }} variant='label2' fontWeight="500" color='foregroundSecondary' >
-            {state}
-        </Text>
-    )
-}
+  return (
+    <Text
+      style={{ fontFamily }}
+      variant="label2"
+      fontWeight="500"
+      color="foregroundSecondary"
+    >
+      {state}
+    </Text>
+  );
+};
