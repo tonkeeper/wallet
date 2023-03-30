ce73c7c493d032c36dd1cb1195ea69734afab2a8
bogoslavskiy <luice96@yahoo.com>
2023-03-30 20:31:14 +0400

    bump(android): react native 0.71.4

--- packages/mobile/android/app/src/main/java/com/ton_keeper/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/ton_keeper/MainApplication.java
@@ -1,99 +1,79 @@
 package com.ton_keeper;
 
 import android.app.Application;
-import android.content.Context;
 import android.content.res.Configuration;
-
 import androidx.annotation.NonNull;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-import com.ton_keeper.legacy.TonPbkdf2Package;
-import com.ton_keeper.newarchitecture.MainApplicationReactNativeHost;
-import com.ton_keeper.walletstore.WalletStorePackage;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
 
+import com.ton_keeper.legacy.TonPbkdf2Package;
+import com.ton_keeper.walletstore.WalletStorePackage;
+
+import java.util.List;
+
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost host = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
+  private final ReactNativeHost mReactNativeHost =
+    new ReactNativeHostWrapper(this, new DefaultReactNativeHost(this) {
+      @Override
+      public boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+      }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-            ArrayList<ReactPackage> packages = new PackageList(this).getPackages();
-            packages.add(new TonPbkdf2Package());
-            packages.add(new WalletStorePackage());
-            return packages;
-        }
+      @Override
+      protected List<ReactPackage> getPackages() {
+        @SuppressWarnings("UnnecessaryLocalVariable")
+        List<ReactPackage> packages = new PackageList(this).getPackages();
+        packages.add(new TonPbkdf2Package());
+        packages.add(new WalletStorePackage());
+        return packages;
+      }
 
-        @Override
-        protected String getJSMainModuleName() {
-            return "index";
-        }
-    };
+      @Override
+      protected String getJSMainModuleName() {
+        return "index";
+      }
 
-    private final ReactNativeHost mReactNativeHost =
-            new ReactNativeHostWrapper(this, host);
+      @Override
+      protected boolean isNewArchEnabled() {
+        return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+      }
 
-    private final ReactNativeHost mNewArchitectureNativeHost =
-            new ReactNativeHostWrapper(this, new MainApplicationReactNativeHost(this));
+      @Override
+      protected Boolean isHermesEnabled() {
+        return BuildConfig.IS_HERMES_ENABLED;
+      }
+  });
 
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            return mNewArchitectureNativeHost;
-        } else {
-            return mReactNativeHost;
-        }
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      DefaultNewArchitectureEntryPoint.load();
     }
+    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    ApplicationLifecycleDispatcher.onApplicationCreate(this);
+  }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        // If you opted-in for the New Architecture, we enable the TurboModule system
-        ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        SoLoader.init(this, false);
-
-        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-        ApplicationLifecycleDispatcher.onApplicationCreate(this);
-    }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
-    }
-
-    /**
-     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-     */
-    private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
-        if (BuildConfig.DEBUG) {
-            try {
-                // We use reflection here to pick up the class that initializes Flipper,
-                // since Flipper library is not available in release mode
-                Class<?> aClass = Class.forName("com.ton_keeper.ReactNativeFlipper");
-                aClass
-                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-                        .invoke(null, context, reactInstanceManager);
-            } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+  @Override
+  public void onConfigurationChanged(@NonNull Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
+  }
 }
