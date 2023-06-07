3729fb82cce149cc13776cbd503a8ebd41621835
bogoslavskiy <luice96@yahoo.com>
2023-06-08 03:18:52 +0400

    wip(mobile): refactoring browser screen

--- packages/mobile/src/core/index.ts
+++ packages/mobile/src/core/index.ts
@@ -36,3 +36,4 @@ export { Staking } from './Staking/Staking';
 export { StakingPools } from './StakingPools/StakingPools';
 export { StakingPoolDetails } from './StakingPoolDetails/StakingPoolDetails';
 export { StakingSend } from './StakingSend/StakingSend';
+export { BrowserScreen } from './DAppsExplore/BrowserScreen';
