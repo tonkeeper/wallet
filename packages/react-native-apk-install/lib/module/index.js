1db0be87ab594a7846d17a980b9aa24c93cfb0c9
Max Voloshinskii <me@voloshinskii.ru>
2023-06-22 03:26:50 +0400

    wip

---
+++ packages/react-native-apk-install/lib/module/index.js
@@ -0,0 +1,17 @@
+import { NativeModules, Platform } from 'react-native';
+const LINKING_ERROR = `The package 'react-native-apk-install' doesn't seem to be linked. Make sure: \n\n` + Platform.select({
+  ios: "- You have run 'pod install'\n",
+  default: ''
+}) + '- You rebuilt the app after installing the package\n' + '- You are not using Expo Go\n';
+const ApkInstall = NativeModules.ApkInstall ? NativeModules.ApkInstall : new Proxy({}, {
+  get() {
+    throw new Error(LINKING_ERROR);
+  }
+});
+export function installApk(path) {
+  if (Platform.OS === 'ios') {
+    return Promise.reject(new Error('Not supported on iOS'));
+  }
+  return ApkInstall.installApk(path);
+}
+//# sourceMappingURL=index.js.map
---
+++ packages/react-native-apk-install/lib/typescript/index.d.ts
@@ -0,0 +1,2 @@
+export declare function installApk(path: string): Promise<number>;
+//# sourceMappingURL=index.d.ts.map
