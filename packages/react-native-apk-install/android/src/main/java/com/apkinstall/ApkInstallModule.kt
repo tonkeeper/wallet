e05d6c3b6bbc3660510fe2600dc64d3da08e3e83
Max Voloshinskii <me@voloshinskii.ru>
2023-06-22 04:30:12 +0400

    Revert "wip"
    This reverts commit f71620edc605379cd31a375cbbe2166f36f62602.

--- packages/react-native-apk-install/android/src/main/java/com/apkinstall/ApkInstallModule.kt
+++ packages/react-native-apk-install/android/src/main/java/com/apkinstall/ApkInstallModule.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Binder
 import android.os.Build
+import androidx.core.content.FileProvider
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -20,7 +21,21 @@ class ApkInstallModule(reactContext: ReactApplicationContext) : ReactContextBase
 
   @ReactMethod
   fun installApk(path: String, promise: Promise) {
-
+    val toInstall: File = File(path)
+    if (Build.VERSION.SDK_INT >= 24) {
+      val callingPackageName: String? = _context.getPackageManager().getNameForUid(Binder.getCallingUid())
+      val apkUri = FileProvider.getUriForFile(_context, "$callingPackageName.fileprovider", toInstall)
+      val intent = Intent(Intent.ACTION_INSTALL_PACKAGE)
+      intent.setData(apkUri)
+      intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+      _context.startActivity(intent)
+    } else {
+      val apkUri = Uri.fromFile(toInstall)
+      val intent = Intent(Intent.ACTION_VIEW)
+      intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+      _context.startActivity(intent)
+    }
   }
 
 
