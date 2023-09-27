da58bd0955d9024659bdcff56406518e6db9e23e
bogoslavskiy <luice96@yahoo.com>
2023-09-27 18:55:16 +0400

    chore(mobile): change eslint config

--- packages/mobile/.eslintrc.js
+++ packages/mobile/.eslintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['react', 'react-native', 'react-hooks', 'import'],
+  settings: {
+    'react-native/style-sheet-object-names': [
+      'Steezy',
+    ],
+  },
+  rules: {
+    'react-native/no-unused-styles': 1,
+    'react-native/no-inline-styles': 1,
+    'react-native/no-color-literals': 1,
+    'react-native/no-raw-text': 1,
+    'react-native/no-single-element-style-arrays': 1,
+
+    'react-hooks/rules-of-hooks': 1,
+    'react-hooks/exhaustive-deps': 1,
+
+    'import/no-default-export': 1,
+    'import/no-unused-modules': 1,
+    'import/no-cycle': [2, { ignoreExternal: true }],
+  },
 };
