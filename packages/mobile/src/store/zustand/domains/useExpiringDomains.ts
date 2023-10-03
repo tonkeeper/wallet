20fa0e2ac2ab68c29c83dabd8767217d0f1ae98c
bogoslavskiy <luice96@yahoo.com>
2023-10-03 15:16:11 +0400

    fix(mobile): period for expiring domains

--- packages/mobile/src/store/zustand/domains/useExpiringDomains.ts
+++ packages/mobile/src/store/zustand/domains/useExpiringDomains.ts
@@ -19,7 +19,7 @@ export const useExpiringDomains = create(
         try {
           const { data } = await Tonapi.getExpiringDNS({
             account_id,
-            period: 366,
+            period: 30,
           });
 
           const domains = {};
