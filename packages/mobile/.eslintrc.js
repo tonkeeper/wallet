3ca828802f5a0c000af1e9f2bccfcb85216dc76b
bogoslavskiy <luice96@yahoo.com>
2023-10-14 17:05:19 +0400

    fix(mobile): eslint config

--- packages/mobile/.eslintrc.js
+++ packages/mobile/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
