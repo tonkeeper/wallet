ad6de7459343b33e49b2c3152bb265dfa1f3577a
bogoslavskiy <luice96@yahoo.com>
2023-10-18 17:10:41 +0400

    fix(android): build

--- packages/mobile/android/devkit/build.gradle.kts
+++ packages/mobile/android/devkit/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "7.4.2" apply false
     id("com.android.library") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
 }
