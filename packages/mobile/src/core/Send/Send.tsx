a9ce7f9960dbdeb6773eaef0135c2c12e7609dfe
Max Voloshinskii <me@voloshinskii.ru>
2024-01-19 18:25:04 +0300

    Update Send.tsx

--- packages/mobile/src/core/Send/Send.tsx
+++ packages/mobile/src/core/Send/Send.tsx
@@ -57,6 +57,8 @@ import {
 import { getTimeSec } from '$utils/getTimeSec';
 import { Toast } from '$store';
 
+const tokensWithAllowedEncryption = [TokenType.TON, TokenType.Jetton];
+
 export const Send: FC<SendProps> = ({ route }) => {
   const {
     currency: initialCurrency,
@@ -178,7 +180,7 @@ export const Send: FC<SendProps> = ({ route }) => {
         tokenType: nextTokenType,
         currencyAdditionalParams: nextCurrencyAdditionalParams,
       });
-      if (nextTokenType === TokenType.Inscription) {
+      if (!tokensWithAllowedEncryption.includes(nextTokenType)) {
         setCommentEncrypted(false);
       }
     },
@@ -450,7 +452,7 @@ export const Send: FC<SendProps> = ({ route }) => {
         <StepViewItem id={SendSteps.ADDRESS}>
           {(stepProps) => (
             <AddressStep
-              enableEncryption={tokenType !== TokenType.Inscription}
+              enableEncryption={tokensWithAllowedEncryption.includes(tokenType)}
               recipient={recipient}
               decimals={decimals}
               stepsScrollTop={stepsScrollTop}
