ac795256d00ce07358966c9348f22d427e476201
Andrey Sorokin <sorokin0andrey@gmail.com>
2024-02-08 21:40:26 +0600

    add warning toast for testnet & watchonly

--- packages/uikit/src/utils/copyText.ts
+++ packages/uikit/src/utils/copyText.ts
@@ -1,10 +1,16 @@
 import Clipboard from '@react-native-community/clipboard';
 import { t } from '@tonkeeper/shared/i18n';
 import { Toast } from '../components/Toast';
+import { tk } from '@tonkeeper/mobile/src/wallet';
 
 export const copyText = (value?: string | boolean, toastMessage?: string) => () => {
   if (value) {
     Clipboard.setString(String(value));
-    Toast.success(toastMessage ?? t('copied'));
+    const message = toastMessage ?? t('copied');
+    if (tk.wallet?.isTestnet || tk.wallet?.isWatchOnly) {
+      Toast.warning(message);
+    } else {
+      Toast.success(message);
+    }
   }
 };
