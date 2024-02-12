b5014d2054f26c6da5d5e5ba54ea8b390bcaa5a5
Andrey Sorokin <sorokin0andrey@gmail.com>
2024-02-12 19:15:18 +0600

    update wallet state on switch

--- packages/mobile/src/wallet/Tonkeeper.ts
+++ packages/mobile/src/wallet/Tonkeeper.ts
@@ -420,6 +420,7 @@ export class Tonkeeper {
 
   public switchWallet(identifier: string) {
     const wallet = this.wallets.get(identifier);
+    this.wallet.preload();
     this.setWallet(wallet!);
   }
 
