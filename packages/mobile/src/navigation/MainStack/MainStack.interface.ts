a9db0caca10710cbd34451a91a006021ce14147c
Max Voloshinskii <me@voloshinskii.ru>
2024-03-04 16:33:01 +0300

    fix(mobile): Fix Holders TonConnect (#748)

--- packages/mobile/src/navigation/MainStack/MainStack.interface.ts
+++ packages/mobile/src/navigation/MainStack/MainStack.interface.ts
@@ -31,7 +31,7 @@ export type MainStackParamList = {
   [MainStackRouteNames.ManageTokens]: {};
   [MainStackRouteNames.AddressUpdateInfo]: {};
   [MainStackRouteNames.AddWatchOnlyStack]: {};
-  [MainStackRouteNames.HoldersWebView]: {};
+  [MainStackRouteNames.HoldersWebView]: { path?: string };
   [MainStackRouteNames.ChangePin]: {};
   [MainStackRouteNames.ResetPin]: {};
   [MainStackRouteNames.ChangePinBiometry]: {};
