3d3472ba0d7e012f7a83d52c1d55360a2cf47298
Max Voloshinskii <me@voloshinskii.ru>
2024-03-12 14:56:55 +0300

    add more pools for demo

--- packages/mobile/src/core/DevMenu/DevMenu.tsx
+++ packages/mobile/src/core/DevMenu/DevMenu.tsx
@@ -55,22 +55,22 @@ export const DevMenu: FC = () => {
     );
   }, [addNotification]);
 
-  const handleShowRestakeBanner = useCallback(() => {
-    addNotification(
-      {
-        name: 'Tonkeeper',
-        icon_url: 'https://tonkeeper.com/assets/apps/tonkeeper.png',
-        message: 'Withdraw from Queue #2 please 🥺',
-        received_at: Date.now(),
-        deeplink: 'ton://staking',
-      },
-      tk.wallet.address.ton.raw,
-    );
-    tk.wallet.staking.toggleRestakeBanner(
-      true,
-      '0:efbc198fdf051c8e85cf6358c77d3e3e7e06f6f788a65581f910774b9c029e7a',
-    );
-  }, [addNotification]);
+  const handleShowRestakeBanner = useCallback(
+    (address: string, name: string) => () => {
+      addNotification(
+        {
+          name: 'Tonkeeper',
+          icon_url: 'https://tonkeeper.com/assets/apps/tonkeeper.png',
+          message: `Withdraw from ${name} please 🥺`,
+          received_at: Date.now(),
+          deeplink: 'ton://staking',
+        },
+        tk.wallet.address.ton.raw,
+      );
+      tk.wallet.staking.toggleRestakeBanner(true, address);
+    },
+    [addNotification],
+  );
 
   const {
     devFeatures,
@@ -195,7 +195,27 @@ export const DevMenu: FC = () => {
           <List.Item onPress={handlePushNotification} title="Push notification" />
         </List>
         <List>
-          <List.Item onPress={handleShowRestakeBanner} title="Show restake banner" />
+          <List.Item
+            onPress={handleShowRestakeBanner(
+              '0:efbc198fdf051c8e85cf6358c77d3e3e7e06f6f788a65581f910774b9c029e7a',
+              'Queue #2',
+            )}
+            title="Show restake banner (queue #2)"
+          />
+          <List.Item
+            onPress={handleShowRestakeBanner(
+              '0:00ff9fdd8b3b80d70e8ea734d262f5e1bd4c184c33535bf3190dd67408629e7a',
+              'Queue #1',
+            )}
+            title="Show restake banner (queue #1)"
+          />
+          <List.Item
+            onPress={handleShowRestakeBanner(
+              '0:98e0ce936589ce327181425aa2fe7c9e6880752de0457c680fe2e5d0d555a16a',
+              'Whales',
+            )}
+            title="Show restake banner (whales)"
+          />
         </List>
       </Screen.ScrollView>
     </Screen>
