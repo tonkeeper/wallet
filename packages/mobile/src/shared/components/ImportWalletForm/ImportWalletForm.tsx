948b472150189df7511042fb450b6679318f0210
Max Voloshinskii <me@voloshinskii.ru>
2024-04-30 00:06:14 +0300

    fix(mobile): Copy/paste mnemonic improvements

--- packages/mobile/src/shared/components/ImportWalletForm/ImportWalletForm.tsx
+++ packages/mobile/src/shared/components/ImportWalletForm/ImportWalletForm.tsx
@@ -1,7 +1,9 @@
 import React, { FC, useCallback, useMemo, useRef, useState } from 'react';
 import Animated, {
   useAnimatedScrollHandler,
+  useAnimatedStyle,
   useSharedValue,
+  withTiming,
 } from 'react-native-reanimated';
 import { TapGestureHandler } from 'react-native-gesture-handler';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -9,21 +11,22 @@ import { useDispatch } from 'react-redux';
 
 import { deviceHeight, isAndroid, ns, parseLockupConfig } from '$utils';
 import { InputItem } from './InputItem';
-import { Button, Input, NavBarHelper, Text } from '$uikit';
+import { Input, NavBarHelper, Text, Button } from '$uikit';
 import * as S from './ImportWalletForm.style';
 import { useReanimatedKeyboardHeight } from '$hooks/useKeyboardHeight';
 import { ImportWalletFormProps } from './ImportWalletForm.interface';
 import { useInputsRegistry } from './useInputRegistry';
 import { WordHintsPopup, WordHintsPopupRef } from './WordHintsPopup';
-import { Keyboard } from 'react-native';
+import { Keyboard, KeyboardAvoidingView } from 'react-native';
 import { wordlist } from '$libs/Ton/mnemonic/wordlist';
 import { Toast } from '$store';
 import { t } from '@tonkeeper/shared/i18n';
+import { Steezy, Button as ButtonNew } from '@tonkeeper/uikit';
+import Clipboard from '@react-native-community/clipboard';
 
 export const ImportWalletForm: FC<ImportWalletFormProps> = (props) => {
   const { onWordsFilled } = props;
 
-  
   const { bottom: bottomInset } = useSafeAreaInsets();
   const dispatch = useDispatch();
   const inputsRegistry = useInputsRegistry();
@@ -36,6 +39,7 @@ export const ImportWalletForm: FC<ImportWalletFormProps> = (props) => {
   const [isConfigInputShown, setConfigInputShown] = useState(false);
   const [config, setConfig] = useState('');
   const [isRestoring, setRestoring] = useState(false);
+  const hasTouchedInputs = useSharedValue(false);
 
   const deferredScrollToInput = useRef<((offset: number) => void) | null>(null);
   const { keyboardHeight } = useReanimatedKeyboardHeight({
@@ -57,8 +61,17 @@ export const ImportWalletForm: FC<ImportWalletFormProps> = (props) => {
     setConfig(text);
   }, []);
 
+  const pasteButtonStyle = useAnimatedStyle(
+    () => ({
+      opacity: withTiming(hasTouchedInputs.value ? 0 : 1, { duration: 200 }),
+    }),
+    [],
+  );
+
   const handleMultipleWords = useCallback((index: number, text: string) => {
+    hasTouchedInputs.value = true;
     const words = text
+      .replace(/\r\n|\r|\n/g, ' ')
       .split(' ')
       .map((word) => word.trim())
       .filter((word) => word.length > 0);
@@ -76,6 +89,16 @@ export const ImportWalletForm: FC<ImportWalletFormProps> = (props) => {
       inputsRegistry.getRef(cursor - 1)?.focus();
     }
   }, []);
+
+  const handlePasteButton = useCallback(async () => {
+    if (hasTouchedInputs.value) {
+      return;
+    }
+    const maybePhrase = await Clipboard.getString();
+    if (maybePhrase.replace(/\r\n|\r|\n/g, ' ').split(' ').length === 24) {
+      handleMultipleWords(0, maybePhrase);
+    }
+  }, [handleMultipleWords]);
 
   const handleSpace = useCallback((index: number) => {
     if (index === 24) {
@@ -185,6 +208,7 @@ export const ImportWalletForm: FC<ImportWalletFormProps> = (props) => {
 
   const handleChangeText = useCallback(
     (index: number) => (text: string) => {
+      hasTouchedInputs.value = true;
       const overlap = 10;
       const offsetTop = inputsRegistry.getPosition(index) + S.INPUT_HEIGHT - overlap;
       const contentWidth = isAndroid ? 0 : inputsRegistry.getContentWidth(index);
@@ -269,6 +293,31 @@ export const ImportWalletForm: FC<ImportWalletFormProps> = (props) => {
           </Button>
         </S.ButtonWrap>
       </Animated.ScrollView>
+      <KeyboardAvoidingView>
+        <Animated.View
+          style={[
+            styles.pasteButtonContainer.static,
+            { bottom: bottomInset + 16 },
+            pasteButtonStyle,
+          ]}
+        >
+          <ButtonNew
+            onPress={handlePasteButton}
+            color="tertiary"
+            size="medium"
+            title={t('paste')}
+          />
+        </Animated.View>
+      </KeyboardAvoidingView>
     </>
   );
 };
+
+const styles = Steezy.create({
+  pasteButtonContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+  },
+});
