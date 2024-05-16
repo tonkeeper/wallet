09c0eb6430f42e4edb3c9433375faba4693f1422
Max Voloshinskii <me@voloshinskii.ru>
2024-05-16 16:40:03 +0300

    fix (#862)

--- packages/@core-js/src/utils/network/network.ts
+++ packages/@core-js/src/utils/network/network.ts
@@ -1,5 +1,5 @@
-import { BackoffOptions } from "./backoff";
-import { jsonToUrl } from "./network.utils";
+import { BackoffOptions } from './backoff';
+import { jsonToUrl } from './network.utils';
 
 type NetworkMethods = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
@@ -7,7 +7,7 @@ export type NetworkOptions = {
   params?: Record<string, any>;
   backoff?: BackoffOptions | boolean;
   headers?: any;
-}
+};
 
 export type NetworkResponse<TData = any> = {
   statusText: string;
@@ -15,37 +15,43 @@ export type NetworkResponse<TData = any> = {
   status: number;
   ok: boolean;
   data: TData;
-}
+};
 
 export function createNetworkMethod(method: NetworkMethods) {
-  return async <T = any>(url: string, options: NetworkOptions): Promise<NetworkResponse<T>>  => {
+  return async <T = any>(
+    url: string,
+    options: NetworkOptions,
+  ): Promise<NetworkResponse<T>> => {
     const fetchOptions: RequestInit = { method, headers: options.headers ?? {} };
 
     let query: string | undefined;
     if (options.params) {
       if (method === 'GET') {
         query = jsonToUrl(options.params);
-      } else  {
+      } else {
         fetchOptions.body = JSON.stringify(options.params);
-        (fetchOptions.headers as Record<string, any>)['Content-Type'] = 'application/json';
+        (fetchOptions.headers as Record<string, any>)['Content-Type'] =
+          'application/json';
       }
     }
 
     const endpoint = url + (query ? `?${query}` : '');
     const response = await fetch(endpoint, fetchOptions);
-    let data = {} as T;
-    try {
-      data = await response.json();
-    } catch (err) {}
 
-    return { 
+    if (response.status >= 400) {
+      throw new Error('Bat response');
+    }
+
+    let data = (await response.json()) as T;
+
+    return {
       statusText: response.statusText,
       headers: response.headers,
       status: response.status,
       ok: response.ok,
-      data, 
+      data,
     };
-  }
+  };
 }
 
 export const network = {
